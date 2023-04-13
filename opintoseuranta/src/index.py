c6853eab0efeb182160567a713d245db99f60a6a
Veetihei <veetiheikkinen99@gmail.com>
2023-04-13 14:49:04 +0300

    Added login userinterface

---
+++ opintoseuranta/src/index.py
@@ -0,0 +1,15 @@
+from tkinter import Tk
+from ui.ui import UI
+
+def main():
+    window = Tk()
+    window.title("Opintoseuranta")
+    
+    ui = UI(window)
+    ui.start()
+    
+    window.mainloop()
+    
+
+if __name__ == "__main__":
+    main()
